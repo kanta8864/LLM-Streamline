ebdfe4bb71c569357a6c518bfa41f54bed05e6e7
kanta8864 <kantadelft@gmail.com>
2025-06-13 13:20:21 +0200

    appstainer loading failed

--- run_job.sh
+++ run_job.sh
@@ -18,10 +18,7 @@ set -e
 echo "Loading modules..."
 module use /opt/insy/modulefiles
 module load cuda/12.1
-# FIX-1: 'singularity' is often the name for the apptainer module on HPCs.
-# If this also fails, try 'module spider apptainer' or 'module spider singularity'
-# to find the correct name, or remove this line if it's in your default path.
-module load apptainer
+
 
 # --- Job Execution & Path Definitions ---
 echo "Job started on $(hostname)"
